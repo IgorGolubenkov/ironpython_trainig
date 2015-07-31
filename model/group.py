f32d833ab60fd16d7dbfdb65145152985b6e4060
Igor Golubenkov <i.golubenkov@mail.ru>
2015-07-31 17:33:56 +0300

    начала работы над irompython

---
+++ model/group.py
@@ -0,0 +1,18 @@
+
+
+class Group:
+
+    def __init__(self, name=None, header=None, footer=None, id=None):
+        self.name = name
+
+
+    def __repr__(self):
+        return self.name
+
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+    def key(self):
+        self.name
---
+++ test_somthing.py
@@ -0,0 +1,4 @@
+
+
+def test_somthing():
+    print("ok")
